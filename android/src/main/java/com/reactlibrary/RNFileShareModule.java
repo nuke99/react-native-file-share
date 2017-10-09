b6a545e8130bdb1e8d2fb2c8de3f84638efc8f17
Theepika <iyertheebi13@gmail.com>
2017-10-09 15:59:29 +0530

    Local files removed

--- android/src/main/java/com/reactlibrary/RNFileShareModule.java
+++ android/src/main/java/com/reactlibrary/RNFileShareModule.java
@@ -26,28 +26,4 @@ public class RNFileShareModule extends ReactContextBaseJavaModule {
     return "RNFileShare";
   }
   
-  @ReactMethod
-  public void ShareImage(){
-    Intent share = new Intent(Intent.ACTION_SEND_MULTIPLE);
-
-    File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+ "/Cat.jpg");
-    File path2 = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+ "/Dog.jpg");
-
-    Uri uri = Uri.fromFile(path);
-    Uri uri2 = Uri.fromFile(path2);
-
-    ArrayList<Uri> mylist = new ArrayList<Uri>();
-    mylist.add(uri);
-    mylist.add(uri2);
-
-    Intent shareIntent = new Intent();
-    shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
-    shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, mylist);
-    shareIntent.setType("image/*");
-    getCurrentActivity().startActivity(Intent.createChooser(shareIntent, "Share images to.."));
-
-  }
-
-
-
 }
